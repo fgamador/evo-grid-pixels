f552b028537671f75f09958c86b4e5299496e329
Franz Amador <franzamador@gmail.com>
2024-12-18 12:13:37 -0800

    Overwrite with Conway example code

--- src/main.rs
+++ src/main.rs
@@ -2,35 +2,56 @@
 #![forbid(unsafe_code)]
 
 use error_iter::ErrorIter as _;
-use log::error;
+use evo_grid::world::ConwayGrid;
+use log::{debug, error};
 use pixels::{Error, Pixels, SurfaceTexture};
-use winit::dpi::LogicalSize;
-use winit::event::{Event, WindowEvent};
-use winit::event_loop::EventLoop;
-use winit::keyboard::KeyCode;
-use winit::window::{Window, WindowBuilder};
+use winit::{
+    dpi::LogicalSize,
+    event::{Event, WindowEvent},
+    event_loop::EventLoop,
+    keyboard::KeyCode,
+    window::WindowBuilder,
+};
 use winit_input_helper::WinitInputHelper;
-use evo_grid::world::World;
 
-const WIDTH: u32 = 320;
-const HEIGHT: u32 = 240;
+const WIDTH: u32 = 400;
+const HEIGHT: u32 = 300;
 
 fn main() -> Result<(), Error> {
     env_logger::init();
     let event_loop = EventLoop::new().unwrap();
-    let window = build_window(&event_loop);
-    let mut pixels = build_pixels(&window)?;
-    let mut world = World::new(WIDTH, HEIGHT);
     let mut input = WinitInputHelper::new();
 
+    let window = {
+        let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
+        let scaled_size = LogicalSize::new(WIDTH as f64 * 3.0, HEIGHT as f64 * 3.0);
+        WindowBuilder::new()
+            .with_title("Conway's Game of Life")
+            .with_inner_size(scaled_size)
+            .with_min_inner_size(size)
+            .build(&event_loop)
+            .unwrap()
+    };
+
+    let mut pixels = {
+        let window_size = window.inner_size();
+        let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
+        Pixels::new(WIDTH, HEIGHT, surface_texture)?
+    };
+
+    let mut life = ConwayGrid::new_random(WIDTH as usize, HEIGHT as usize);
+    let mut paused = false;
+
+    let mut draw_state: Option<bool> = None;
+
     let res = event_loop.run(|event, elwt| {
-        // Draw the current frame
+        // The one and only event that winit_input_helper doesn't have for us...
         if let Event::WindowEvent {
             event: WindowEvent::RedrawRequested,
             ..
         } = event
         {
-            world.draw(pixels.frame_mut());
+            life.draw(pixels.frame_mut());
             if let Err(err) = pixels.render() {
                 log_error("pixels.render", err);
                 elwt.exit();
@@ -38,14 +59,74 @@ fn main() -> Result<(), Error> {
             }
         }
 
-        // Handle input events
+        // For everything else, for let winit_input_helper collect events to build its state.
+        // It returns `true` when it is time to update our game state and request a redraw.
         if input.update(&event) {
             // Close events
             if input.key_pressed(KeyCode::Escape) || input.close_requested() {
                 elwt.exit();
                 return;
             }
+            if input.key_pressed(KeyCode::KeyP) {
+                paused = !paused;
+            }
+            if input.key_pressed_os(KeyCode::Space) {
+                // Space is frame-step, so ensure we're paused
+                paused = true;
+            }
+            if input.key_pressed(KeyCode::KeyR) {
+                life.randomize();
+            }
+            // Handle mouse. This is a bit involved since support some simple
+            // line drawing (mostly because it makes nice looking patterns).
+            let (mouse_cell, mouse_prev_cell) = input
+                .cursor()
+                .map(|(mx, my)| {
+                    let (dx, dy) = input.cursor_diff();
+                    let prev_x = mx - dx;
+                    let prev_y = my - dy;
 
+                    let (mx_i, my_i) = pixels
+                        .window_pos_to_pixel((mx, my))
+                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                    let (px_i, py_i) = pixels
+                        .window_pos_to_pixel((prev_x, prev_y))
+                        .unwrap_or_else(|pos| pixels.clamp_pixel_pos(pos));
+
+                    (
+                        (mx_i as isize, my_i as isize),
+                        (px_i as isize, py_i as isize),
+                    )
+                })
+                .unwrap_or_default();
+
+            if input.mouse_pressed(0) {
+                debug!("Mouse click at {mouse_cell:?}");
+                draw_state = Some(life.toggle(mouse_cell.0, mouse_cell.1));
+            } else if let Some(draw_alive) = draw_state {
+                let release = input.mouse_released(0);
+                let held = input.mouse_held(0);
+                debug!("Draw at {mouse_prev_cell:?} => {mouse_cell:?}");
+                debug!("Mouse held {held:?}, release {release:?}");
+                // If they either released (finishing the drawing) or are still
+                // in the middle of drawing, keep going.
+                if release || held {
+                    debug!("Draw line of {draw_alive:?}");
+                    life.set_line(
+                        mouse_prev_cell.0,
+                        mouse_prev_cell.1,
+                        mouse_cell.0,
+                        mouse_cell.1,
+                        draw_alive,
+                    );
+                }
+                // If they let go or are otherwise not clicking anymore, stop drawing.
+                if release || !held {
+                    debug!("Draw end");
+                    draw_state = None;
+                }
+            }
             // Resize the window
             if let Some(size) = input.window_resized() {
                 if let Err(err) = pixels.resize_surface(size.width, size.height) {
@@ -54,29 +135,13 @@ fn main() -> Result<(), Error> {
                     return;
                 }
             }
-
-            // Update internal state and request a redraw
-            world.update();
+            if !paused || input.key_pressed_os(KeyCode::Space) {
+                life.update();
+            }
             window.request_redraw();
         }
     });
     res.map_err(|e| Error::UserDefined(Box::new(e)))
-}
-
-fn build_window(event_loop: &EventLoop<()>) -> Window {
-    let size = LogicalSize::new(WIDTH as f64, HEIGHT as f64);
-    WindowBuilder::new()
-        .with_title("Hello Pixels")
-        .with_inner_size(size)
-        .with_min_inner_size(size)
-        .build(&event_loop)
-        .unwrap()
-}
-
-fn build_pixels(window: &Window) -> Result<Pixels, Error> {
-    let window_size = window.inner_size();
-    let surface_texture = SurfaceTexture::new(window_size.width, window_size.height, &window);
-    Pixels::new(WIDTH, HEIGHT, surface_texture)
 }
 
 fn log_error<E: std::error::Error + 'static>(method_name: &str, err: E) {
